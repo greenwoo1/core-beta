b9bbd274e64b340b8f9f17afea6e9c8e596193d5
Anton Mazur <toxas99@icloud.com>
2025-04-29 02:37:06 +0300

    Finish creating migrations, add three new Models, add two new Factories and three Seeders

--- database/migrations/2025_04_27_220656_create_products_table.php
+++ database/migrations/2025_04_27_220656_create_products_table.php
@@ -13,7 +13,18 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('slug')->unique();
+            $table->string('title')->unique();
+            $table->string('SKU',35)->unique();
+            $table->text('description')->nullable();
+            $table->float('price')->unsigned()->startingValue(1);
+            $table->unsignedTinyInteger('discount')->nullable();
+            $table->unsignedSmallInteger('quantity')->default(0);
+            $table->text('thumbnail');
             $table->timestamps();
+
+            $table->fullText(['slug']);
+            $table->fullText(['title']);
         });
     }
 
--- database/migrations/2025_04_28_150537_create_images_table.php
+++ database/migrations/2025_04_28_150537_create_images_table.php
@@ -11,15 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('category_product', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')
-                ->constrained('categories')
-                ->cascadeOnDelete();
-
-            $table->foreignId('product_id')
-                ->constrained('products')
-                ->cascadeOnDelete();
+            $table->string('path');
+            $table->morphs('imageable');
             $table->timestamps();
         });
     }
@@ -29,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('category_product');
+        Schema::dropIfExists('images');
     }
 };
